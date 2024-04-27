98f347524635c67995d292d1bf70a2003f609547
timcbuck <timcbuck.94@gmail.com>
2024-04-27 16:44:09 +0930

    Finish creating 5 round game

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let humanScore, computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
 
 
 function getComputerChoice() {
@@ -16,35 +18,41 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == computerChoice) {
         // draw
-        console.log("draw");
+        alert("It's a draw! The computer also chose " + computerChoice + ".");
     } else {
         switch(humanChoice) {
             case "rock":
                 if (computerChoice == "paper") {
                     // lose
-                    console.log("lose");
+                    alert("You lose! Rock is beaten by paper.");
+                    updateScore("computer");
                 } else {
                     // win
-                    console.log("win");
+                    alert("You win! Rock beats scissors.");
+                    updateScore("human");
                 }
                 break;
             case "paper":
                 if (computerChoice == "scissors") {
                     // lose
-                    console.log("lose");
+                    alert("You lose! Paper is beaten by scissors.");
+                    updateScore("computer");
                     break;
                 } else {
                     // win
-                    console.log("win");
+                    alert("You win! Paper beats rock.");
+                    updateScore("human");
                 }
                 break;
             case "scissors":
                 if (computerChoice == "rock") {
                     // lose
-                    console.log("lose");
+                    alert("You lose! Scissors is beaten by rock.");
+                    updateScore("computer");
                 } else {
                     // win
-                    console.log("win");
+                    alert("You win! Scissors beats paper.");
+                    updateScore("human");
                 }
                 break;
         }
@@ -54,7 +62,50 @@ function playRound(humanChoice, computerChoice) {
 
 function game(humanChoice) {
     let computerChoice = getComputerChoice();
-    console.log("Human choice: ", humanChoice);
-    console.log("Computer choice: ", computerChoice);
+    console.log(computerChoice);
     playRound(humanChoice, computerChoice);
+    finishGame();
+    round += 1;
+    console.log(round);
+    let el = document.getElementById("roundNumber");
+    el.textContent = "Round: " + round;
+}
+
+
+function updateScore(winner) {
+    if (winner == "human") {
+        humanScore += 1;
+        let el = document.getElementById("humanScore");
+        el.textContent = "Human score: " + humanScore;
+    } else if (winner == "computer") {
+        computerScore += 1;
+        let el = document.getElementById("computerScore");
+        el.textContent = "Computer score: " + computerScore;
+    }
+}
+
+function checkWinner(humanScore, computerScore) {
+    if (humanScore == computerScore) {
+        return "draw";
+    } else if (humanScore > computerScore) {
+        return "win";
+    } else {
+        return "lose";
+    }
+}
+
+
+function finishGame() {
+    let result = checkWinner(humanScore, computerScore);
+    if (round == 5) {
+        if (result == "draw") {
+            alert("That's the end of the game. It's a draw!");
+        } else if (result == "win") {
+            alert("Congratulations, you won the game!");
+        } else {
+            alert("Too bad! You lost the game!");
+        }
+        location.reload();
+    }
+    
 }
